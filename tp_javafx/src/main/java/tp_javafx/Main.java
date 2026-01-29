6c9fbdb7b6c56b6e85358c564f3a9606d613b6e7
JulienGrade <gradejulien@gmail.com>
2026-01-29 09:03:10 +0100

    core: Add structure and Main.java for tp_javafx

---
+++ tp_javafx/src/main/java/tp_javafx/Main.java
@@ -0,0 +1,23 @@
+package tp_javafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 720, 480);
+
+        stage.setTitle("TP RDV - JavaFx running");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
